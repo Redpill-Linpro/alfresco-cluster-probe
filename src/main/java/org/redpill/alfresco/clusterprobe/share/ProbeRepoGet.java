426109de8b125e2e2bda4599b26199d98150fc57
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2015-09-07 11:11:27 +0200

    Added a repository probe to share so that share monitoring can be connected to one backend node

--- src/main/java/org/redpill/alfresco/clusterprobe/share/ProbeRepoGet.java
+++ src/main/java/org/redpill/alfresco/clusterprobe/share/ProbeRepoGet.java
@@ -4,8 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.alfresco.error.StackTraceUtil;
 import org.apache.log4j.Logger;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.redpill.alfresco.clusterprobe.AbstractProbe;
 import org.redpill.alfresco.clusterprobe.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +17,9 @@ import org.springframework.extensions.webscripts.connector.ConnectorService;
 import org.springframework.extensions.webscripts.connector.Response;
 import org.springframework.stereotype.Component;
 
-@Component("webscript.org.redpill.alfresco.clusterprobe.probe.get")
-public class ProbeGet extends AbstractProbe {
-  private static final Logger LOG = Logger.getLogger(ProbeGet.class);
+@Component("webscript.org.redpill.alfresco.clusterprobe.proberepo.get")
+public class ProbeRepoGet extends AbstractProbe {
+  private static final Logger LOG = Logger.getLogger(ProbeRepoGet.class);
   protected static final String ENDPOINT_ID = "alfresco";
 
   @Autowired
@@ -31,7 +29,6 @@ public class ProbeGet extends AbstractProbe {
   @Override
   protected Settings getProbeSettings() {
     try {
-      final String server = getServer();
 
       final RequestContext requestContext = ThreadLocalRequestContext.getRequestContext();
 
@@ -43,15 +40,11 @@ public class ProbeGet extends AbstractProbe {
 
       final Connector connector = connService.getConnector(ENDPOINT_ID, currentUserId, currentSession);
 
-      final String alfrescoURL = "/org/redpill/alfresco/clusterprobe/settings?server=" + server;
+      final String alfrescoURL = "/org/redpill/alfresco/clusterprobe/probe";
 
       final Response response = connector.call(alfrescoURL);
 
-      final String jsonResponse = response.getResponse();
-
-      final JSONObject json = new JSONObject(new JSONTokener(jsonResponse));
-
-      return new Settings(json.getString("text"), json.getInt("code"));
+      return new Settings(response.getResponse(), response.getStatus().getCode());
     } catch (final Exception ex) {
       LOG.error(ex.getMessage(), ex);
       final StringBuilder sb = new StringBuilder();
